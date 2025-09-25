ba55e384465644ea08fdf76985f0d73da9b02fa2
Aluisio Lima <aaluisio309@gmail.com>
2025-09-24 21:25:15 -0300

    melhorias

--- src/calc_media/CalcMedia.java
+++ src/calc_media/CalcMedia.java
@@ -18,10 +18,14 @@ public class CalcMedia {
             }
             notas[i - 1] = nota;
         }
-        double mediaB1 = calcularMediaBimestral(notas[0], notas[1], notas[2], notas[3]);
-        double mediaB2 = calcularMediaBimestral(notas[4], notas[5], notas[6], notas[7]);
+        double mediaSemestre1 = calcularMediaSemestral(notas[0], notas[1], notas[2], notas[3]);
+        double mediaSemestre2 = calcularMediaSemestral(notas[4], notas[5], notas[6], notas[7]);
 
-        double mediaSemestre1 = calcularMediaSemestral(mediaB1, mediaB2);
+        double mediaB1 = calcularMediaBimestral(notas[0], notas[1]);
+        double mediaB2 = calcularMediaBimestral(notas[2], notas[3]);
+        double mediaB3 = calcularMediaBimestral(notas[4], notas[5]);
+        double mediaB4 = calcularMediaBimestral(notas[6], notas[7]);
+
 
         double mediaAnual = calcularMediaAnual(
             notas[0], notas[1], notas[2], notas[3],
@@ -32,21 +36,26 @@ public class CalcMedia {
         System.out.println("Média 1º Bimestre: " + mediaB1);
         System.out.println("Média 2º Bimestre: " + mediaB2);
         System.out.println("Média do 1º Semestre: " + mediaSemestre1);
-        System.out.println("Média Anual: " + mediaAnual);
+        System.out.println("--------------------");
+        System.out.println("Média 3º Bimestre: " + mediaB3);
+        System.out.println("Média 4º Bimestre: " + mediaB4);
+        System.out.println("Média do 2º Semestre: " + mediaSemestre2);
+        System.out.println("--------------------");
+        System.out.println("Média Final: " + mediaAnual);
         System.out.println("--------" + (mediaAnual >= 7 ? " Aprovado" : " Reprovado") + "----------");
 
         scanner.close();
     }
 
-    public static double calcularMediaBimestral(double n1, double n2, double n3, double n4) {
+    public static double calcularMediaSemestral(double n1, double n2, double n3, double n4) {
         return (n1 + n2 + n3 + n4) / 4;
     }
 
-    public static double calcularMediaSemestral(double n1, double n2) {
+    public static double calcularMediaBimestral(double n1, double n2) {
         return (n1 + n2) / 2;
     }
 
     public static double calcularMediaAnual(double n1, double n2, double n3, double n4, double n5, double n6, double n7, double n8) {
-        return (calcularMediaBimestral(n1, n2, n3, n4) + calcularMediaBimestral(n5, n6, n7, n8)) / 2;
+        return (calcularMediaSemestral(n1, n2, n3, n4) + calcularMediaSemestral(n5, n6, n7, n8)) / 2;
     }
 }
